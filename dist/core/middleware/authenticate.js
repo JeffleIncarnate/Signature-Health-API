b87acc08d1ee9b2ab4e98e4a8f68188cf1d39463
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-08 21:48:08 +1200

    feat: build

---
+++ dist/core/middleware/authenticate.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authenticateRequest = authenticateRequest;
+function authenticateRequest(req, res, next) { }
+//# sourceMappingURL=authenticate.js.map
