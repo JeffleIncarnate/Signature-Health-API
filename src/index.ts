5f14366492d12a2e790add3e4a26a19b6e1576d4
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-09 09:20:09 +1200

    feat: bro nah

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,8 @@ app.post(
     }
 
     const transporter = nodemailer.createTransport({
-      service: "hotmail",
+      host: "smtp-mail.outlook.com",
+      port: 587,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
