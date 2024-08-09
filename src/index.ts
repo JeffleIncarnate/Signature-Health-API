2c160d0b12e2272a1250ffba351af92b72920094
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-09 13:53:28 +1200

    feat: maybe fix?

--- src/index.ts
+++ src/index.ts
@@ -61,6 +61,14 @@ app.post(
       },
     });
 
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
+
     try {
       const success = await transporter.sendMail({
         from: process.env.EMAIL_USER,
